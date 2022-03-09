ede32afcc5a7f6d21801bd1869763b75bd524567
Mark D <MLDimitry@gmail.com>
2022-03-09 11:11:13 -0500

    Update aliases.zsh

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -4,3 +4,4 @@
 alias sc='systemctl'
 alias jc='journalctl'
 alias setupThemes='rake dev:theme_lifecycle:setup'
+alias runTestsAsCI='CI=true BUILDKITE_PARALLEL_JOB="1"  BUILDKITE_PARALLEL_JOB_COUNT="16" ./node_modules/.bin/sewing-kit test --no-graphql'
